ada2830d269238b9ac424dbb383018ffe3adc237
vtzc <vzubicueta@usm.cl>
2025-10-13 15:31:49 -0300

    D

--- Semana9/core/models.py
+++ Semana9/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=6, primary_key=True)
+    nombre = models.CharField()
+    precio = models.IntegerField()
+    stock = models.IntegerField(min=0)
+
+class Proveedor(models.Model):
+   producto = models.ForeignKey(Producto) 
+   
+   
