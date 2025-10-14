b4bdb5364bb501c4412ff6cc4b83f0518f1b1abc
vtzc <vzubicueta@usm.cl>
2025-10-14 10:31:39 -0300

    F

--- Lab 4/INFJMC/core/models.py
+++ Lab 4/INFJMC/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Docente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
--- Semana9/core/models.py
+++ Semana9/core/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 class Producto(models.Model):
     codigo = models.CharField(max_length=6, primary_key=True)
-    nombre = models.CharField()
+    nombre = models.CharField(max_length=30)
     precio = models.IntegerField()
     stock = models.IntegerField(min=0)
+    
+    def __str__(self):
+        return self.nombre
 
 class Proveedor(models.Model):
    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
